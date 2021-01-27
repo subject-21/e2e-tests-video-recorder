dce7796f8bab4235df217e95d9e84ea24190b45b
subject-21 <fefenaim13@gmail.com>
2021-01-27 15:31:23 +0200

    remove tests files

--- src/test/testRecordings.js
+++
@@ -1,8 +0,0 @@
-const Recorder = require("../recorder/record");
-const rec = new Recorder("C:/Users/Neili/OneDrive/Documents/projects/e2e-tests-video-recorder/results", "mp4");
-
-rec.start();
-
-setTimeout(() => {
-    rec.stop();
-}, 6000);
