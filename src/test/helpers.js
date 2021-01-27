fbedd01acdf6d583a08571c76fab6c544ad2f5e0
subject-21 <fefenaim13@gmail.com>
2021-01-27 15:21:27 +0200

    add package.json keywords

--- src/test/helpers.js
+++
@@ -1,8 +0,0 @@
-const AllureReporter = require("jasmine-allure-reporter");
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
-const jasmineEnv = jasmine.getEnv();
-const reporter = new AllureReporter({
-    resultsDir: "reporter-results"
-});
-
-jasmineEnv.addReporter(reporter);
